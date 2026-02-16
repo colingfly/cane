6f640bd843ab4af86ca31731978321a8d9ea18a0
colingly <colingfly@gmail.com>
2026-02-15 20:43:44 -0500

    re-enable healthcheck

--- fix_toml.py
+++ fix_toml.py
@@ -2,5 +2,7 @@
     f.write('[build]\n')
     f.write('dockerfilePath = "Dockerfile"\n')
     f.write('[deploy]\n')
+    f.write('healthcheckPath = "/api/health"\n')
+    f.write('healthcheckTimeout = 600\n')
     f.write('restartPolicyType = "ON_FAILURE"\n')
     f.write('restartPolicyMaxRetries = 3\n')
